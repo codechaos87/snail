8a12b79c9ec4b2a7008f0bef1f893fce33ffd270
chaos <chaos@172.19.30.11>
2018-01-10 09:04:56 +0800

    add ok

---
+++ src/main/java/m01/Test.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:snail
+ * File Name:Test.java
+ * Package Name:m01
+ * Date:2018年1月10日上午9:04:26
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package m01;
+/**
+ * Description:   <br/>
+ * Date:     2018年1月10日 上午9:04:26 <br/>
+ * @author   chaos
+ * @version
+ * @see
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        //  Auto-generated method stub
+
+    }
+
+}
+
