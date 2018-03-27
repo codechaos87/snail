75a72ff5c7387f0b574f0c397abc3b431f27360c
Dajingya <864716371@qq.com>
2018-03-27 23:02:57 +0800

    asdf

---
+++ src/main/java/m03/d27/wangjing/2.java
@@ -0,0 +1,60 @@
+/**
+ * Project Name:Demo
+ * File Name:info2.java
+ * Package Name:Demojdbc
+ * Date:2018年3月27日上午10:36:34
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package Demojdbc;
+
+/**
+ * Description: <br/>
+ * Date: 2018年3月27日 上午10:36:34 <br/>
+ * 
+ * @author l
+ * @version
+ * @see
+ */
+public class info2 {
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    private int cid;
+
+    private String cname;
+
+    private int pid;
+
+    private String pname;
+
+}
